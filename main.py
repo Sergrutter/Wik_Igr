e65121fbb20859234c4e6a48e555decc0b5d3de7
iLLokMaster <romapridieb@gmail.com>
2025-04-01 17:02:24 +0300

    форма для поиска главной страницы, заготовка для шаблона страницы. баг: Не отображается русский язык

--- main.py
+++ main.py
@@ -11,7 +11,20 @@ def read_file(file_path):
 
 @app.route('/')
 def welcome():
-    return read_file('welcome.html')
+    """Главная страница"""
+    return read_file('home.html')
+
+
+@app.route('/search', methods = ['GET', 'POST'])
+def search():
+    """страница поиска других страниц"""
+    return read_file('search.html')
+
+
+@app.route('/text_block')
+def text_block():
+    """Страница на какую-то тему"""
+    return read_file('text_block.html')
 
 
 if __name__ == '__main__':
