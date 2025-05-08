0ca4bf92d56b1551c8a6070ce73ece12ad4e41f2
Sergrutter <134686706+Sergrutter@users.noreply.github.com>
2025-05-08 20:36:59 +0300

    Update db.py

--- db.py
+++ db.py
@@ -21,4 +21,15 @@ class User(db.Model):
     name = db.Column(db.String(100), nullable = False)
 
 
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    page_id = db.Column(db.Integer, db.ForeignKey('page.id'), nullable=False)
+
+    user = db.relationship('User', backref='comments')
+    page = db.relationship('Page', backref='comments')
+
+
 db.create_all()
