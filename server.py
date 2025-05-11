e3f5a252c02f7d0f60a18f9a7ef81e544200c5ea
Sergrutter <134686706+Sergrutter@users.noreply.github.com>
2025-05-11 08:20:41 +0300

    Update server.py

--- server.py
+++ server.py
@@ -119,9 +119,9 @@ def home():
     return render_template('home.html', pages=pages)
 
 @app.route('/profile/<username>')
-    def profile(username):
-        user = User.query.filter_by(username=username).first_or_404()
-        return render_template('profile.html', user=user)
+def profile(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    return render_template('profile.html', user=user)
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
